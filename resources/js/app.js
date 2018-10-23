da908975c2f5246d055d02244d059a07210f999c
elatfimohamed <mohamedelatfi93@gmial.com>
2018-10-23 19:24:48 +0200

    Sync

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,27 +4,37 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import './bootstrap'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import ExempleComponent from './components/ExampleComponent.vue'
+import Tasks from './components/Tasks.vue'
+
+require('./bootstrap')
 
 // ES6/VUE/BABEL -> imports
 // window OCO en browser Objecte global
-window.Vue = require('vue');
-Vue.use(require('vuetify'));
+window.Vue = Vue
+
+window.Vue.use(Vuetify)
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-Vue.component('tasks', require('./components/Tasks.vue'));
+Vue.component('example-component', ExempleComponent)
+Vue.component('tasks', Tasks)
 
 const app = new Vue({
-    el: '#app',
-    data: () => ({
-        drawer: null
-    }),
-    props: {
-        source: String
-    }
+  el: '#app',
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
 })
