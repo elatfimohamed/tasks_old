c8cc3ba24b0cf4c2f6ad306b60d4b182cde8e37e
elatfimohamed <mohamedelatfi93@gmial.com>
2018-10-24 17:51:20 +0200

    Sync

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -1,68 +1,95 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: elatfi
- * Date: 19/10/18
- * Time: 16:17
- */
 
 namespace Tests\Unit;
+
 use App\File;
+use App\Tag;
 use App\Task;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 class TaskTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function ca_assign_user_to_task()
+    /**
+     * @test
+     */
+    public function can_assign_user_to_task()
     {
-        $task= Task::create([
-            'name' => 'Compra pa'
-
+        // 1 Prepare
+        $task = Task::create([
+            'name' => 'Comprar pa'
         ]);
 
         $userOriginal = factory(User::class)->create();
 
+        // 2 Execute
         $task->assignUser($userOriginal);
 
-        user = $task->user;
+        $user = $task->user;
 
         $this->assertTrue($user->is($userOriginal));
-
     }
 
-    public function can_assign_tag_to_taks()
+    /**
+     * @test
+     */
+    public function can_assign_tag_to_task()
     {
+        // 1 Prepare
+        $task = Task::create([
+            'name' => 'Comprar pa'
+        ]);
+
+        $tag = Tag::create([
+            'name' => 'home'
+        ]);
+
+        // execució
+        $task->addTag($tag);
+
+        // Assertion
+        $tags = $task->tags;
+
+        $this->assertTrue($tags[0]->is($tag));
     }
 
-
-
-
+    /**
+     * @test
+     */
     public function a_task_can_have_tags() {
         // 1 Prepare
         $task = Task::create([
             'name' => 'Comprar pa'
         ]);
+
         $tag1 = Tag::create([
             'name' => 'home'
         ]);
         $tag2 = Tag::create([
             'name' => 'work'
         ]);
+
         $tag3 = Tag::create([
             'name' => 'studies'
         ]);
+
         $tags = [$tag1, $tag2, $tag3];
+
         // execució
-        $task->assignTags($tags);
+        $task->addTags($tags);
+
         // Assertion
         $tags = $task->tags;
+
         $this->assertTrue($tags[0]->is($tag1));
         $this->assertTrue($tags[1]->is($tag2));
         $this->assertTrue($tags[2]->is($tag3));
     }
+
+
     /**
      * @test
      */
@@ -72,21 +99,29 @@ class TaskTest extends TestCase
         $task = Task::create([
             'name' => 'Comprar pa'
         ]);
+
         $fileOriginal = File::create([
             'path' => 'fitxer1.pdf'
         ]);
+
 //        add_file_to_task($file, $task);
         $task->assignFile($fileOriginal);
+
         // 2 Executo -> Wishful programming
+
         // IMPORTANT 2 maneres
         // 1 Aixó torna tota la relació, treball extra
 //        $file = $task->files()->where('path','');
         // 2 Això retorna el object:
+
         $file = $task->file;
+
         // 3 Comprovo
         // $file
         $this->assertTrue($file->is($fileOriginal));
+
     }
+
     /**
      * @test
      */
@@ -97,9 +132,44 @@ class TaskTest extends TestCase
             'name' => 'Comprar pa'
         ]);
         // 2 Executo -> Wishful programming
-        $file = $task->file();
+        $file = $task->file;
+
         // 3 Comprovo
         // $file
         $this->assertNull($file);
+
     }
+
+    /**
+     * @test
+     */
+    public function can_toggle_completed()
+    {
+        $task = factory(Task::class)->create([
+            'completed' => false
+        ]);
+        $task->toggleCompleted();
+        $this->assertTrue($task->completed);
+
+        $task = factory(Task::class)->create([
+            'completed' => true
+        ]);
+        $task->toggleCompleted();
+        $this->assertFalse($task->completed);
+    }
+
+
+        /**
+         * @test
+         */
+        public function map()
+        {
+
+            
+
+
+
+        }
+
+}
 }
